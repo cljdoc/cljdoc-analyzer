325a60b867d3fa34fc44e52cad468143c048d513
lread <lee@dlread.com>
2019-08-21 20:03:26 -0400

    tweak temporary test script

--- test.sh
+++ test.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
-
 # TODO: Delete me before release
 
+set -euo pipefail
 
-read -r -d '' OPTS << EOF
-{:project "lread/cljdoc-exerciser"
- :version "1.0.19"
- :jarpath "http://repo.clojars.org/lread/cljdoc-exerciser/1.0.19/cljdoc-exerciser-1.0.19.jar"
- :pompath "http://repo.clojars.org/lread/cljdoc-exerciser/1.0.19/cljdoc-exerciser-1.0.19.pom"}
-EOF
+function analyze() {
+    local group=$1
+    local artifact=$2
+    local version=$3
+    local repo_prefix="http://repo.clojars.org/${group}/${artifact}/${version}/${artifact}-${version}"
+    OPTS=$(cat <<EOM
+{:project "${group}/${artifact}"
+ :version "${version}"
+ :jarpath "${repo_prefix}.jar"
+ :pompath "${repo_prefix}.pom"}
+EOM
+)
+    echo ${OPTS}
+    clojure --report stderr -m cljdoc.analysis.runner-ng "${OPTS}"
+}
 
-#clojure --report stderr -m cljdoc.analysis.runner-ng "{:project \"lread/cljdoc-exerciser\" :version \"1.0.19\" :jarpath \"http://repo.clojars.org/lread/cljdoc-exerciser/1.0.19/cljdoc-exerciser-1.0.19.jar\" :pompath \"http://repo.clojars.org/lread/cljdoc-exerciser/1.0.19/cljdoc-exerciser-1.0.19.pom\"}"
-
-clojure --report stderr -m cljdoc.analysis.runner-ng "${OPTS}"
+#analyze "lread" "cljdoc-exerciser" "1.0.19"
+analyze "cljfmt" "cljfmt" "0.6.4"
