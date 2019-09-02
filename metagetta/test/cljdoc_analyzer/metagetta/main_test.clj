ac11568e9fa30214c0747e55c4d201c6ea920906
lread <lee@dlread.com>
2019-09-02 16:10:39 -0400

    Whitespace only

--- metagetta/test/cljdoc_analyzer/metagetta/main_test.clj
+++ metagetta/test/cljdoc_analyzer/metagetta/main_test.clj
@@ -4,102 +4,102 @@
 
 (def base-expected-analysis-result
   '({:name metagetta-test.test-ns1.altered
-    :publics [{:name altered-def-with-absolute-file
-               :type :var
-               :file "metagetta_test/test_ns1/record.cljc"
-               :line 7}
-              {:name altered-fn-with-source-relative-file
-               :arglists ([])
-               :type :var
-               :file "metagetta_test/test_ns1/multimethod.cljc"
-               :line 14}
-              {:name altered-macro-with-root-relative-file
-               :arglists ([])
-               :type :macro
-               :doc "added doc\n"
-               :file "metagetta_test/test_ns1/multimethod.cljc"
-               :line 3}
-              {:arglists ([x]),
-               :doc "Operation 1 docs\n",
-               :file "metagetta_test/test_ns1/protocols.cljc",
-               :line 6,
-               :name fn-pointing-to-protocol-fn,
-               :type :var}]}
-   {:name metagetta-test.test-ns1.macro
-    :publics [{:name macdoc
-               :arglists ([a b c d])
-               :type :macro
-               :doc "Macro docs\n"
-               :file "metagetta_test/test_ns1/macro.cljc"
-               :line 23}
-              {:name simple
-               :arglists ([a b])
-               :type :macro
-               :file "metagetta_test/test_ns1/macro.cljc"
-               :line 11}
-              {:name varargs
-               :arglists ([a & xs])
-               :type :macro
-               :file "metagetta_test/test_ns1/macro.cljc"
-               :line 17}]}
-   {:name metagetta-test.test-ns1.multiarity
-    :publics [{:name multiarity
-               :arglists ([] [a] [a b] [a b c d])
-               :type :var
-               :doc "Multiarity comment\n"
-               :file "metagetta_test/test_ns1/multiarity.cljc"
-               :line 7}]}
-   {:name metagetta-test.test-ns1.multimethod
-    :publics [{:name start
-               :type :multimethod
-               :file "metagetta_test/test_ns1/multimethod.cljc"
-               :line 6}]}
-   {:name metagetta-test.test-ns1.protocols
-    :publics [{:name ProtoTest
-               :type :protocol
-               :doc "Protocol comment.\n"
-               :members ({:arglists ([a]), :name alpha :type :var}
-                         {:arglists ([z]), :name beta :type :var}
-                         {:arglists ([m]), :name matilda :type :var}
-                         {:arglists ([x] [x y])
-                          :doc "Multi args docs\n"
-                          :name multi-args
-                          :type :var}
-                         {:arglists ([x])
-                          :doc "Operation 1 docs\n"
-                          :name operation-one,
-                          :type :var}
-                         {:arglists ([y]), :name zoolander, :type :var})
-               :file "metagetta_test/test_ns1/protocols.cljc"
-               :line 6}]}
-   {:name metagetta-test.test-ns1.record
-    :publics [{:name DefRecordTest
-               :type :var
-               :file "metagetta_test/test_ns1/record.cljc"
-               :line 6}
-              {:name record-test
-               :type :var
-               :file "metagetta_test/test_ns1/record.cljc"
-               :line 8}]}
-   {:name metagetta-test.test-ns1.special-tags
-    :publics [{:name added-fn
-               :arglists ([a b])
-               :type :var
-               :added "10.2.2"
-               :file "metagetta_test/test_ns1/special_tags.cljc"
-               :line 26}
-              {:name deprecated-fn
-               :arglists ([x])
-               :type :var
-               :deprecated "0.4.0"
-               :file "metagetta_test/test_ns1/special_tags.cljc"
-               :line 10}
-              {:name dynamic-def
-               :type :var
-               :doc "dynamic def docs\n"
-               :dynamic true
-               :file "metagetta_test/test_ns1/special_tags.cljc"
-               :line 21}]}))
+     :publics [{:name altered-def-with-absolute-file
+                :type :var
+                :file "metagetta_test/test_ns1/record.cljc"
+                :line 7}
+               {:name altered-fn-with-source-relative-file
+                :arglists ([])
+                :type :var
+                :file "metagetta_test/test_ns1/multimethod.cljc"
+                :line 14}
+               {:name altered-macro-with-root-relative-file
+                :arglists ([])
+                :type :macro
+                :doc "added doc\n"
+                :file "metagetta_test/test_ns1/multimethod.cljc"
+                :line 3}
+               {:arglists ([x]),
+                :doc "Operation 1 docs\n",
+                :file "metagetta_test/test_ns1/protocols.cljc",
+                :line 6,
+                :name fn-pointing-to-protocol-fn,
+                :type :var}]}
+    {:name metagetta-test.test-ns1.macro
+     :publics [{:name macdoc
+                :arglists ([a b c d])
+                :type :macro
+                :doc "Macro docs\n"
+                :file "metagetta_test/test_ns1/macro.cljc"
+                :line 23}
+               {:name simple
+                :arglists ([a b])
+                :type :macro
+                :file "metagetta_test/test_ns1/macro.cljc"
+                :line 11}
+               {:name varargs
+                :arglists ([a & xs])
+                :type :macro
+                :file "metagetta_test/test_ns1/macro.cljc"
+                :line 17}]}
+    {:name metagetta-test.test-ns1.multiarity
+     :publics [{:name multiarity
+                :arglists ([] [a] [a b] [a b c d])
+                :type :var
+                :doc "Multiarity comment\n"
+                :file "metagetta_test/test_ns1/multiarity.cljc"
+                :line 7}]}
+    {:name metagetta-test.test-ns1.multimethod
+     :publics [{:name start
+                :type :multimethod
+                :file "metagetta_test/test_ns1/multimethod.cljc"
+                :line 6}]}
+    {:name metagetta-test.test-ns1.protocols
+     :publics [{:name ProtoTest
+                :type :protocol
+                :doc "Protocol comment.\n"
+                :members ({:arglists ([a]), :name alpha :type :var}
+                          {:arglists ([z]), :name beta :type :var}
+                          {:arglists ([m]), :name matilda :type :var}
+                          {:arglists ([x] [x y])
+                           :doc "Multi args docs\n"
+                           :name multi-args
+                           :type :var}
+                          {:arglists ([x])
+                           :doc "Operation 1 docs\n"
+                           :name operation-one,
+                           :type :var}
+                          {:arglists ([y]), :name zoolander, :type :var})
+                :file "metagetta_test/test_ns1/protocols.cljc"
+                :line 6}]}
+    {:name metagetta-test.test-ns1.record
+     :publics [{:name DefRecordTest
+                :type :var
+                :file "metagetta_test/test_ns1/record.cljc"
+                :line 6}
+               {:name record-test
+                :type :var
+                :file "metagetta_test/test_ns1/record.cljc"
+                :line 8}]}
+    {:name metagetta-test.test-ns1.special-tags
+     :publics [{:name added-fn
+                :arglists ([a b])
+                :type :var
+                :added "10.2.2"
+                :file "metagetta_test/test_ns1/special_tags.cljc"
+                :line 26}
+               {:name deprecated-fn
+                :arglists ([x])
+                :type :var
+                :deprecated "0.4.0"
+                :file "metagetta_test/test_ns1/special_tags.cljc"
+                :line 10}
+               {:name dynamic-def
+                :type :var
+                :doc "dynamic def docs\n"
+                :dynamic true
+                :file "metagetta_test/test_ns1/special_tags.cljc"
+                :line 21}]}))
 
 
 (def expected-cljs-analysis-result
@@ -121,11 +121,11 @@
 (def expected-clj-analysis-result
   (concat
    '({:name metagetta-test.cljs-macro-functions.foo.core,
-     :publics ({:arglists ([a b]),
-                :file "metagetta_test/cljs_macro_functions/foo/core.clj",
-                :line 6,
-                :name add,
-                :type :macro})})
+      :publics ({:arglists ([a b]),
+                 :file "metagetta_test/cljs_macro_functions/foo/core.clj",
+                 :line 6,
+                 :name add,
+                 :type :macro})})
    base-expected-analysis-result))
 
 ;; TODO: not testing namespaces option yet
