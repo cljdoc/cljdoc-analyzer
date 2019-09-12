b806b79eeb1df517c46a2be2e47ef58ec0984069
lread <lee@dlread.com>
2019-09-12 17:18:19 -0400

    Try to encourage cljdoc-analyzer to find metagetta
    Would like cljdoc-analyzer to find metagetta as a project embedded in
    cljdoc-analyzer. Possible?

--- src/cljdoc_analyzer/deps.clj
+++ src/cljdoc_analyzer/deps.clj
@@ -36,7 +36,7 @@
          deps-map))
 
 (def cljdoc-analyzer-metagetta-dep
-  {'cljdoc-analyzer/reader {:local/root "metagetta"}})
+  {'cljdoc-analyzer/reader {:local/root "./metagetta"}})
 
 (defn- extra-pom-deps
   "Some projects require additional depenencies that have either been specified with
