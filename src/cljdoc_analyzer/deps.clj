1e675fb9e1f0f7546064cb0ad51a09d6aec092d2
Lee Read <lee@dlread.com>
2023-02-24 14:28:27 -0500

    Be less naive when parsing poms (#69)
    Use tools deps which uses Maven libs to parse and evaluate pom.
    
    We are using tools deps internals.
    If that becomes a maintenance headache, we will adapt, but for now it is
    convenient.
    
    Other than parsing poms via maven, the algorithm remains unchanged, we
    still, for example:
    - only look at non-transitive deps
    - do not consider dep exclusions for provided deps
    
    We'll adjust if needed, but might not need to.
    
    Added an integration test for org.clojure/tools.deps to cover one former
    problematic use case.
    
    Closes #66
    Closes cljdoc/cljdoc#742

--- src/cljdoc_analyzer/deps.clj
+++ src/cljdoc_analyzer/deps.clj
@@ -1,11 +1,20 @@
 (ns ^:no-doc cljdoc-analyzer.deps
-  (:require [babashka.fs :as fs]
-            [clojure.java.io :as io]
-            [clojure.tools.deps :as tdeps]
-            [clojure.string :as string]
-            [version-clj.core :as v]
-            [cljdoc-shared.pom :as pom]
-            [cljdoc-shared.proj :as proj]))
+  (:require
+   [babashka.fs :as fs]
+   [cljdoc-shared.proj :as proj]
+   [clojure.java.io :as io]
+   [clojure.string :as string]
+   [clojure.tools.deps :as tdeps]
+   [version-clj.core :as v]
+   ;; the following not part of the tools deps public API, will reconsider if this causes us any grief
+   [clojure.tools.deps.extensions.pom :as tdeps-pom]
+   [clojure.tools.deps.util.maven :as tdeps-maven]
+   [clojure.tools.deps.util.session :as tdeps-session])
+  (:import
+   [org.apache.maven.model Dependency Model Repository]
+   [org.apache.maven.model.building StringModelSource]))
+
+(set! *warn-on-reflection* true)
 
 (defn- ensure-recent-ish [deps-map]
   (let [min-versions {'org.clojure/clojure "1.9.0"
@@ -40,64 +49,84 @@
                     target-jar)
                   (io/resource "metagetta"))}})
 
-(defn- extra-pom-deps
-  "Some projects require additional depenencies that have either been specified with
-  scope 'provided', 'system', 'test', are marked 'optional' or are specified via documentation, e.g. a README.
-  Maybe should be able to configure this via their git repo user cljdoc.edn configuration
-  file but this situation being an edge case this is a sufficient fix for now."
-  [pom]
-  (->> (:dependencies pom)
-       ;; compile/runtime scopes will be included by the normal dependency resolution.
-       (filter #(or (#{"provided" "system" "test"} (:scope %))
-                    (:optional %)))
-       ;; The version can be nil when pom's utilize
-       ;; dependencyManagement this unsurprisingly breaks tools.deps
-       ;; Remains to be seen if this causes any issues
-       ;; http://maven.apache.org/guides/introduction/introduction-to-dependency-mechanism.html#Dependency_Management
-       (remove #(nil? (:version %)))
-       (remove #(string/starts-with? (:artifact-id %) "boot-"))
-       ;; Ensure that tools.reader version is used as specified by CLJS
-       (remove #(and (= (:group-id %) "org.clojure")
-                     (= (:artifact-id %) "tools.reader")))
-       (map (fn [{:keys [group-id artifact-id version]}]
-               [(symbol group-id artifact-id) {:mvn/version version}]))
+(defn- model-dep->data
+  "Taken from clojure/tools.deps `clojure.tools.deps.extensions.pom` and modified to not bother with gathering exclusions."
+  [^Dependency dep]
+  (let [scope (.getScope dep)
+        optional (.isOptional dep)
+        artifact-id (.getArtifactId dep)
+        classifier (.getClassifier dep)]
+    [(symbol (.getGroupId dep) (if (string/blank? classifier) artifact-id (str artifact-id "$" classifier)))
+     (cond-> {:mvn/version (.getVersion dep)}
+       scope (assoc :scope scope)
+       optional (assoc :optional true))]))
+
+(defn non-transitive-deps
+  [^Model pom]
+  (->> (.getDependencies pom)
+       (map model-dep->data)))
+
+(defn provided-deps
+  "Returns a deps map of provided, system, test scoped and optional deps from `deps-vec`"
+  [deps-vec]
+  (->> deps-vec
+       (filter (fn [[_project coord]]
+                 (or (#{"provided" "system" "test"} (:scope coord))
+                     (:optional coord))))
+       ;; remove any problematic provided deps
+       (remove (fn [[project _coord]] (= 'org.clojure/tools.reader project)))
+       (remove (fn [[project _coord]] (string/starts-with? (name project) "boot-")))
        (into {})))
 
 (defn- clj-cljs-deps
-  [pom]
-  (->> (:dependencies pom)
-       (map (fn [{:keys [group-id artifact-id version]}]
-              [(symbol group-id artifact-id) {:mvn/version version}]))
-       (filter #(-> % first #{'org.clojure/clojure 'org.clojure/clojurescript}))
+  "Returns a deps map of clojure and/or clojurescript deps from `deps-vec`"
+  [deps-vec]
+  (->> deps-vec
+       (filter (fn [[project _coord]]
+                 (-> project  #{'org.clojure/clojure 'org.clojure/clojurescript})))
        (into {})))
 
 (defn- pom-repos
-  [pom]
-  (->> (:repositories pom)
-       (map (fn [repo] [(:id repo) (dissoc repo :id)]))
+  "Returns maven repositories declared with `pom`"
+  [^Model pom]
+  (->> (.getRepositories pom)
+       (map (fn [^Repository repo] [(.getId repo) {:url (.getUrl repo)}]))
        (into {})))
 
-(defn- deps
-  "Create a deps.edn style :deps map for the project specified by the
-  Jsoup document `pom`."
+(defn- overriding-deps
+  "Returns a deps map from `pom` that is used as overrides for the actual deps in `pom`."
   [pom metagetta-dep compensating-deps]
-  (-> (extra-pom-deps pom)
-      (merge (clj-cljs-deps pom))
-      (merge compensating-deps)
-      (ensure-required-deps)
-      (ensure-recent-ish)
-      (merge metagetta-dep)))
+  (let [non-transitive-deps (non-transitive-deps pom)] ;; this includes optional, provided, test, etc
+    (->> (merge (provided-deps non-transitive-deps)
+                (clj-cljs-deps non-transitive-deps))
+         (reduce-kv (fn [m project coord]
+                      (assoc m project (dissoc coord :optional :scope :exclusions)))
+                    {})
+         (merge compensating-deps)
+         (ensure-required-deps)
+         (ensure-recent-ish)
+         (merge metagetta-dep))))
+
+(defn read-pom-model
+  "Taken from clojure/tools.deps `clojure.tools.deps.extensions.pom` and modified to support reading from a slurpable-pom."
+  ^Model [slurpable-pom config]
+  (let [pom-str (slurp slurpable-pom)
+        settings (tdeps-session/retrieve :mvn/settings #(tdeps-maven/get-settings))]
+    (tdeps-session/retrieve
+      {:pom :model :source slurpable-pom}
+      #(tdeps-pom/read-model (StringModelSource. pom-str) config settings))))
 
 (defn resolved-deps
-  "Returns resolved deps for `pom-url` and include `jar-url` as one of those deps.
+  "Returns resolved (and tweaked) deps for `pom-url`, includes `jar-url` as a dep.
   Include `compensating-deps` when needed."
   [work-dir jar-url pom-url default-repos extra-repos compensating-deps]
   {:pre [(string? jar-url) (string? pom-url)]}
-  (let [pom (pom/parse (slurp pom-url))
-        project (proj/clojars-id (:artifact-info pom))
+  (let [repos (merge default-repos extra-repos)
+        pom (read-pom-model pom-url {:mvn/repos repos})
+        project (proj/clojars-id {:group-id (.getGroupId pom) :artifact-id (.getArtifactId pom)})
         metagetta-dep (cljdoc-analyzer-metagetta-dep! work-dir)
         repos (merge default-repos (pom-repos pom) extra-repos)]
-    (tdeps/resolve-deps {:deps (deps pom metagetta-dep compensating-deps)
+    (tdeps/resolve-deps {:deps (overriding-deps pom metagetta-dep compensating-deps)
                          :mvn/repos repos}
                         {:extra-deps {(symbol project) {:local/root jar-url}}
                          :verbose false})))
